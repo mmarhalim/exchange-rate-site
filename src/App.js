56607615f58f2bb87ccbf01f9b99f2607e51111a
mmarhalim <115380281+mmarhalim@users.noreply.github.com>
2022-12-13 07:40:54 -0700

    layout structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from './Layout';
+
 import './App.css';
 
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route render={() => <h1>404 Not found</h1>} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
